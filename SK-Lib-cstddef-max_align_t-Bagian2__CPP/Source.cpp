69fd404ed9d5688d5bac2c76337241aaf45832e4
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-05 22:02:35 +0700

    Add project files.

---
+++ SK-Lib-cstddef-max_align_t-Bagian2__CPP/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstddef>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main(){
+    std::cout << alignof(std::max_align_t) << '\n';
+    _getch();
+    return 0;
+}
